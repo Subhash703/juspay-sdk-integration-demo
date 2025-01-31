abb787f4040939803fa66ec5a4ee8ba9c4d8c400
Subhash Chandra <49841848+Subhash703@users.noreply.github.com>
2025-01-31 15:46:39 +0530

    fixed ios integration for brazil (#848)

--- ec-headless-ios-swift-integration-demo/br/juspay-sdk-integration-swift/ViewController.swift
+++ ec-headless-ios-swift-integration-demo/br/juspay-sdk-integration-swift/ViewController.swift
@@ -8,7 +8,7 @@ import UIKit
 // Importing GlobalJuspayPaymentsSDK
 // block:start:import-global-juspay-payments-sdk
 
-import GlobalJuspayPaymentsSDK
+import HyperSDK
 // block:end:import-global-juspay-payments-sdk
 
 
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     // Create an instance of GlobalJuspayPaymentsServices
     // block:start:create-global-juspay-payments-services-instance
 
-    let globalJuspayInstance = GlobalJuspayPaymentsServices();
+    let hyperInstance = HyperServices(tenantId: HyperProperties.shared.customerConfig["tenant"] as! String, clientId: HyperProperties.shared.merchantData.clientId )
     // block:end:create-global-juspay-payments-services-instance
 
     
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
 
         let sdkPayload : [String: Any] = [
             "requestId": UUID().uuidString,
-            "service": "hyperapi",
+            "service": "in.juspay.hyperapi",
             "payload": innerPayload
         ]
 
@@ -52,62 +52,61 @@ class ViewController: UIViewController {
     // This callback will get all events from globalJuspay Instance
     // block:start:create-hyper-callback
 
-    let globalJuspayCallbackHandler : GlobalJuspayPaymentsCallback = { [weak self] (response) in
-    	guard let self = self else {
-        	return
-    	}
-        
-    	if let data = response, let event = data["event"] as? String {
-        	if (event == "hide_loader") {
-            	// hide loader
-        	}
-        	// Handle Process Result
-        	else if (event == "process_result") {
-            	let error = data["error"] as! Bool
-    
-            	let innerPayload = data["payload"] as! [ String: Any ]
-            	let status = innerPayload["status"] as! String
-            	let pi = innerPayload["paymentInstrument"] as? String
-            	let pig = innerPayload["paymentInstrumentGroup"] as? String
-    
-            	if (!error) {
-                	// txn success, status should be "charged"
-                	// process data -- show pi and pig in UI maybe also?
-                	// example -- pi: "PAYTM", pig: "WALLET"
-                	// call orderStatus once to verify (false positives)
-            	} else {
-    
-                	let errorCode = data["errorCode"] as! String
-                	let errorMessage = data["errorMessage"] as! String
-                	switch (status) {
-                    	case "backpressed":
-                        	// user back-pressed from checkout screen without initiating any txn
-                        	break;
-                    	case "user_aborted":
-                        	// user initiated a txn and pressed back
-                        	// poll order status
-                        	break;
-                    	case "pending_vbv", "authorizing":
-                        	// txn in pending state
-                        	// poll order status until backend says fail or success
-                        	break;
-                    	case "authorization_failed", "authentication_failed", "api_failure":
-                        	// txn failed
-                        	// poll orderStatus to verify (false negatives)
-                        	break;
-                    	case "new":
-                        	// order created but txn failed
-                        	// also failure
-                        	// poll order status
-                        	break;
-                    	default:
-                        	// unknown status, this is also failure
-                        	// poll order status
-                        	break;
-                	}
-            	}
-        	}
-    	}
+    func hyperCallbackHandler(response : (Optional<Dictionary<String, Any>>)) {
+        if let data = response, let event = data["event"] as? String {
+            if (event == "hide_loader") {
+                // hide loader
+            }
+            // Handle Process Result
+            // block:start:handle-process-result
+            else if (event == "process_result") {
+                let error = data["error"] as! Bool
+
+                let innerPayload = data["payload"] as! [ String: Any ]
+                let status = innerPayload["status"] as! String
+                let pi = innerPayload["paymentInstrument"] as? String
+                let pig = innerPayload["paymentInstrumentGroup"] as? String
+
+                if (!error) {
+                    // txn success, status should be "charged"
+                    // process data -- show pi and pig in UI maybe also?
+                    // example -- pi: "PAYTM", pig: "WALLET"
+                    // call orderStatus once to verify (false positives)
+                } else {
+
+                    let errorCode = data["errorCode"] as! String
+                    let errorMessage = data["errorMessage"] as! String
+                    switch (status) {
+                        case "backpressed":
+                            // user back-pressed from checkout screen without initiating any txn
+                            break;
+                        case "user_aborted":
+                            // user initiated a txn and pressed back
+                            // poll order status
+                            break;
+                        case "pending_vbv", "authorizing":
+                            // txn in pending state
+                            // poll order status until backend says fail or success
+                            break;
+                        case "authorization_failed", "authentication_failed", "api_failure":
+                            // txn failed
+                            // poll orderStatus to verify (false negatives)
+                            break;
+                        case "new":
+                            // order created but txn failed
+                            // also failure
+                            // poll order status
+                            break;
+                        default:
+                            // unknown status, this is also failure
+                            // poll order status
+                            break;
+                    }
+                }
+            }
+            // block:end:handle-process-result
+        }
+
     }
 
     // block:end:create-hyper-callback
@@ -117,7 +116,7 @@ class ViewController: UIViewController {
         // Calling initiate on hyperService instance to boot up payment engine.
         // block:start:initiate-sdk
         
-        globalJuspayInstance.initiate(
+        self.hyperInstance.initiate(
             self,
             payload: createInitiatePayload(),
             callback: globalJuspayCallbackHandler
@@ -129,8 +128,8 @@ class ViewController: UIViewController {
     // Creating process payload JSON object
     // block:start:process-sdk-call
 
-    if globalJuspayInstance?.isInitialised() ?? false {
-     globalJuspayInstance.process(processPayload)               
+    if self.hyperInstance?.isInitialised() ?? false {
+     self.hyperInstance?.process(processPayload)               
     }
     // block:end:process-sdk-call
     
